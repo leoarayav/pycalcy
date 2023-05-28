1ef90e6507f9c2832157ca1c47e794f5a1af2133
leoarayav <leandro.araya08@inacapmail.cl>
2023-05-27 22:09:34 -0400

    refact(setup): new version for package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyclc',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package for calculating',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
