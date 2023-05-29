6f7a6ca9b2dd984cba5cb40cc6696028500d275f
leoarayav <leandro.araya08@inacapmail.cl>
2023-05-29 00:36:28 -0400

    refactor(__main__): new restruct of calculate()

--- pyclc/__main__.py
+++ pyclc/__main__.py
@@ -2,17 +2,17 @@
 This module is the main module of the package and contains the App class
 """
 
-from typing import Callable
+from typing import Callable, Any
 
 class App:
     """This class contains methods for calculating"""
 
-    def calculate(self, method: Callable, *args) -> float:
+    def calculate(self, method: Callable[..., Any], *args) -> None:
         """
         This method calculates the result of the method passed as an argument
 
         Args:
-            method (Callable): method to calculate
+            method (Callable): method to execute
             *args: arguments for the method
         
         Returns:
