a4725f8af3133564b0ec5ad2e0e2ac54b76a9037
leoarayav <leandro.araya08@inacapmail.cl>
2023-05-29 00:29:28 -0400

    refact/feat(__init__): changes

--- pyclc/__init__.py
+++ pyclc/__init__.py
@@ -1,13 +1,9 @@
 '''
-    PyCLC (python calculation) library is a collection of overall methods calculations used in
+    ### PyCLC - Python Calculations
+    A lightweight library is a collection of overall methods calculations used in
     multiples areas, contains all methods done, where you can use it in your projects or simply
     to learn how to calculate something, this library is open source and free to use
 
-    Usage example:
-        >>> from pyclc import Math
-        >>> m = Math()
-        >>> print(m.factorial(7))
-    
     Project author and maintainer:
         Leo Araya (https://www.github.com/leoarayav)
 '''
@@ -17,9 +13,10 @@ from .maths import Math
 from .geometry import Geometry
 from .physics import Physics
 from .astrology import Astrology
+from .crypt import Encrypt
 from .__main__ import App
 
 __title__ = "pyclc"
 __version__ = "0.0.1"
 __author__ = "Leo Araya"
-__all__ = ['General', 'Math', 'Geometry', 'Physics', 'Astrology']
+__all__ = ['General', 'Math', 'Geometry', 'Physics', 'Astrology', 'Encrypt']
