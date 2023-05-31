0dd78072e5549342751fd3be0ea3d4bbe6b2f876
leoarayav <leandro.araya08@inacapmail.cl>
2023-05-31 13:26:49 -0400

    feat(__main__): new way to this module

--- pyclc/__main__.py
+++ pyclc/__main__.py
@@ -2,12 +2,16 @@
 This module is the main module of the package and contains the App class
 """
 
-from typing import Callable, Any
+import typing as tp
 
 class App:
     """This class contains methods for calculating"""
 
-    def calculate(self, method: Callable[..., Any], *args) -> None:
+    def calculate(
+            self, 
+            method: tp.Callable[..., tp.Any], 
+            *args: tp.Any
+        ) -> None:
         """
         This method calculates the result of the method passed as an argument
 
@@ -19,3 +23,33 @@ class App:
             float: result of the method
         """
         return method(*args)
+    
+    def __repr__(self) -> str:
+        """
+        This method returns the representation string of the class
+
+        Returns:
+            str: string representation of the class
+        """
+        return f"{self.__class__.__name__}()"
+    
+    def __str__(self) -> str:
+        """
+        This method string of the class
+
+        Returns:
+            str: string representation of the class
+        """
+        return self.__repr__()
+    
+    def __call__(self, *args: tp.Any) -> None:
+        """
+        This method calculates the result of the method passed as an argument
+
+        Args:
+            *args: arguments for the method
+
+        Returns:
+            None
+        """
+        return self.calculate(*args)
