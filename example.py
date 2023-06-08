a2220036327309161b5d283f1f21ba3d9bdb6110
leoarayav <leandro.araya08@inacapmail.cl>
2023-06-08 13:58:49 -0400

    feat(examples): new implementation of all mods use

--- example.py
+++
@@ -1,4 +0,0 @@
-'''
-    This is a simples examples of how to use pycalcy
-    Author: Leo Araya (https://www.github.com/leoarayav)
-'''
---
+++ examples.py
@@ -0,0 +1,48 @@
+'''
+    This is a simples examples of how to use pycalcy
+    Author: Leo Araya (https://www.github.com/leoarayav)
+'''
+
+from pycalcy import (
+    App,
+    Geometry,
+    Math,
+    General,
+    Physics,
+    Astrology,
+    Encrypt
+)
+
+'''Initialize the class objects'''
+pycalcy = App()
+
+geometry = Geometry()
+math = Math()
+general = General()
+physics = Physics()
+astrology = Astrology()
+crypt = Encrypt()
+
+'''This is a simple example of how to use pycalcy to calculate the area of a circle'''
+aoc = pycalcy(geometry.area_of_circle, 6)
+print(aoc)
+
+'''This is a simple example of how to check the orbit of a planet'''
+planet = pycalcy(astrology.orbital_period, 7, 6)
+print(planet)
+
+'''This is a simple example of how to crypt a message to binary'''
+message = pycalcy(crypt.encrypt_text_to_binary, 'Hello World')
+print(message)
+
+'''This is a simple example of how to calculate the interpolation of a list'''
+itp = pycalcy(math.interpolation, [2, 5, 6, 9, 24], 6)
+print(itp)
+
+'''This is a simple example of how to calculate the density of a body'''
+dsty = pycalcy(physics.density, 6, 7)
+print(dsty)
+
+'''This is a simple example of how to determinate the IMC of a person'''
+val = pycalcy(general.imc, 75, 1.70)
+print(val)
